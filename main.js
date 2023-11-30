b965fd949abf38fc47317036edae9318e8adb71c
zalec <142818531+z4le3c@users.noreply.github>
2023-11-30 17:26:26 -0600

    add input

--- main.js
+++ main.js
@@ -2,16 +2,27 @@ import { Weather } from "./weather.js"
 
 const fetchForcastWeather = async (_location) => {
   let response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=12515910c17845a6a7d30508233011&q=${_location}&days=3`,
-    { mode: 'cors' }
+  `https://api.weatherapi.com/v1/forecast.json?key=12515910c17845a6a7d30508233011&q=${_location}&days=3`,
+  { mode: 'cors' }
   )
   let data = await response.json()
-  console.log(data)
-  console.log(`Celsius: ${Weather.currTemp(data).c}`)
-  console.log(`Fahrenheit: ${Weather.currTemp(data).f}`)
-  console.log(`Min Celsius: ${Weather.currMinTemp(data).c}`)
-  console.log(`Min Fahrenheit: ${Weather.currMinTemp(data).f}`)
-  console.log(`Wind k: ${Weather.currWindSpeed(data).k}`)
+  return data
 }
 
-fetchForcastWeather('Mexico City')
+// fetchForcastWeather('Mexico City')
+
+const searchButton = document.querySelector('#search-button')
+searchButton.addEventListener('click', async () => {
+  const inErrorMessage = document.querySelector('.error-message')
+  inErrorMessage.textContent = ''
+  const weatherLocInput = document.querySelector('#weather-location')
+  if (weatherLocInput.value === '') return
+  
+  let data = await fetchForcastWeather(weatherLocInput.value)
+  if (data.error) {
+    inErrorMessage.textContent = data.error.message
+    return
+  }
+
+  console.log(data)
+})
